c56ac9263cb666e52fde0949d576803aaeb8ca33
Donatas Bražionis <4donatas4@gmail.com>
2019-06-23 14:32:08 +0300

    Added default message.

--- ResourceServer/Startup.cs
+++ ResourceServer/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -37,6 +38,11 @@ namespace ResourceServer
             }
 
             app.UseMvc();
+
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync(nameof(ResourceServer));
+            });
         }
     }
 }
