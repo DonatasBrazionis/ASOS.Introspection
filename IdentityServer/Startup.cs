2c0cac78c18d9e920a8b61f37b35357ed2de1ac0
Donatas Bražionis <4donatas4@gmail.com>
2019-06-23 11:49:24 +0300

    Added CORS.

--- IdentityServer/Startup.cs
+++ IdentityServer/Startup.cs
@@ -26,17 +26,26 @@ namespace IdentityServer
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseCors(builder =>
+            {
+                builder.AllowAnyOrigin();
+                builder.AllowCredentials();
+                builder.AllowAnyMethod();
+                builder.AllowAnyHeader();
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-            
+
             app.UseStatusCodePages();
 
             app.UseMvc();
