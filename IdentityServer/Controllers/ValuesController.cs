49f7c1cd418834dc0bd3b0a4da02705bb43d3966
Donatas Bražionis <4donatas4@gmail.com>
2019-06-23 14:17:31 +0300

    Updated ValuesController for Authorize testing.

--- IdentityServer/Controllers/ValuesController.cs
+++ IdentityServer/Controllers/ValuesController.cs
@@ -1,45 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IdentityServer.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
+    [Authorize]
     public class ValuesController : ControllerBase
     {
-        // GET api/values
+        // GET /api/values/Get
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public async Task<IActionResult> Get()
         {
-            return new string[] { "value1", "value2" };
+            try
+            {
+                var userId = User?.Identity?.Name;
+                if (userId == null)
+                {
+                    return Forbid();
+                }
+
+                await Task.Delay(10);
+                var result = new { UserId = userId };
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
         }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }
