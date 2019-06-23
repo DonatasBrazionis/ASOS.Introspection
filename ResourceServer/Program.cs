9675539bfc94f8c3d18f4a4ed2c8227d7deb3265
Donatas Bražionis <4donatas4@gmail.com>
2019-06-23 14:29:40 +0300

    Updated server port.

--- ResourceServer/Program.cs
+++ ResourceServer/Program.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace ResourceServer
 {
@@ -19,7 +12,7 @@ namespace ResourceServer
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-            .UseUrls("http://+:5001")
+            .UseUrls("http://+:6000")
             .UseStartup<Startup>();
     }
 }
