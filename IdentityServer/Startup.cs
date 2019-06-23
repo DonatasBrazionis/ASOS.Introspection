e7189a015eb4f1ea1fd9f77d1321459459621e85
Donatas Bražionis <4donatas4@gmail.com>
2019-06-23 11:46:15 +0300

    Added default message.

--- IdentityServer/Startup.cs
+++ IdentityServer/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -35,8 +36,13 @@ namespace IdentityServer
             {
                 app.UseDeveloperExceptionPage();
             }
-           
+
             app.UseMvc();
+
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync(nameof(IdentityServer));
+            });
         }
     }
 }
